44921c82ed928181cb5e79208d89e9cb155ee221
Mpofu Gerald <CHA5016@hca.corpad.net>
2022-06-14 15:28:44 -0500

    renamed classes

--- Vehicle.cs
+++ Vehicle.cs
@@ -5,36 +5,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VehicleDemo {
+namespace VehicleDemo
+{
 
     public class Vehicle
     {
-        public Vehicle(string vehicleType, string powerReqs)
+        public Vehicle(string vehicleClass, string powerReqs)
         {
-            VehicleType = vehicleType;
+            VehicleClass = vehicleClass;
             PowerReqs = powerReqs;
         }
 
-        public string VehicleType { get; set; };
+        public string VehicleClass { get; set; };
         public string PowerReqs { get; set; };
 
     }
 
     public class Car : Vehicle
     {
-        public Car(string carType, string engineType) : base(vehicleType, powerReqs)
+        public Car(string carType, string engineType, int doors, string vehicleClass, string powerReqs) : base(vehicleClass, powerReqs)
         {
             CarType = carType;
             EngineType = engineType;
+            NumberOfDoors = doors;
         }
 
         public string CarType { get; set; };
         public string EngineType { get; set; };
+        int NumberOfDoors { get; set; };
+
+        public const int carWheels = 4;
     }
 
-    public class Motorcycle : Vehicle
+    public class Bike : Vehicle
     {
-        public Motorcycle(string bikeType, string bikeEngine, string vehicleType,string powerReqs) : base(vehicleType, powerReqs)
+        public Bike(string bikeType, string bikeEngine, string vehicleClass, string powerReqs) : base(vehicleClass, powerReqs)
         {
             BikeType = bikeType;
             BikeEngine = bikeEngine;
@@ -46,4 +51,31 @@ namespace VehicleDemo {
         public const int bikeWheels = 2;
     }
 
+    public class IndustrialVehicle : Vehicle
+    {
+        public IndustrialVehicle(string industryVehicleType, string machineType, string machinePowerSource, string vehicleClass, string powerReqs) : base(vehicleClass, powerReqs)
+        {
+            IndustryVehicleType = industryVehicleType;
+            MachineType = machineType;
+            MachinePowerSource = machinePowerSource;
+        }
+
+        public string IndustryVehicleType { get; set; };
+        public string MachineType { get; set; };
+        public string MachinePowerSource { get; set; };
+    }
+
+    public class CommercialVehicle : Vehicle
+    {
+        public CommercialVehicle(string commerceType, int numSeats, int wheels, string vehicleClass, string powerReqs) : base(vehicleClass, powerReqs)
+        {
+            CommerceType = commerceType;
+            NumSeats = numSeats;
+            Wheels = wheels;
+        }
+
+        public int NumSeats { get; set; };
+        public int Wheels { get; set; };
+        public string CommerceType { get; set; };
+    }
 }
